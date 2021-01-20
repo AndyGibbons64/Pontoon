355219b95499394eb536e3f27760d2f5f0f04c92
AndyGibbons64 <60094128+AndyGibbons64@users.noreply.github.com>
2021-01-20 00:44:42 +0000

    Version for demo

--- pontoon.py
+++ pontoon.py
@@ -20,6 +20,7 @@ hand = [None, None, None, None, None]
 dealer = [None, None, None, None, None]
 
 in_game = False
+current_deck = []
 player_stuck = False
 player_total = 0
 dealer_total = 0
@@ -34,28 +35,32 @@ player_funds = 100
 draw = False
 hand_over = False
 first_hand = True
+player_ace_count = 0
+dealer_ace_count = 0
 
 gameDisplay = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption('Pontoon by Andy G')
 
-deck=[(11,'images/ace_of_clubs.png'), (11,'images/ace_of_hearts.png'), (11,'images/ace_of_spades.png'), (11,'images/ace_of_diamonds.png'),
-      (2,'images/2_of_clubs.png'), (2,'images/2_of_hearts.png'), (2,'images/2_of_spades.png'), (2,'images/2_of_diamonds.png'),
-      (3,'images/3_of_clubs.png'), (3,'images/3_of_hearts.png'), (3,'images/3_of_spades.png'), (3,'images/3_of_diamonds.png'),
-	  (4,'images/4_of_clubs.png'), (4,'images/4_of_hearts.png'), (4,'images/4_of_spades.png'), (4,'images/4_of_diamonds.png'),
-	  (5,'images/5_of_clubs.png'), (5,'images/5_of_hearts.png'), (5,'images/5_of_spades.png'), (5,'images/5_of_diamonds.png'),
-	  (6,'images/6_of_clubs.png'), (6,'images/6_of_hearts.png'), (6,'images/6_of_spades.png'), (6,'images/6_of_diamonds.png'),
-	  (7,'images/7_of_clubs.png'), (7,'images/7_of_hearts.png'), (7,'images/7_of_spades.png'), (7,'images/7_of_diamonds.png'),
-	  (8,'images/8_of_clubs.png'), (8,'images/8_of_hearts.png'), (8,'images/8_of_spades.png'), (8,'images/8_of_diamonds.png'),
-	  (9,'images/9_of_clubs.png'), (9,'images/9_of_hearts.png'), (9,'images/9_of_spades.png'), (9,'images/9_of_diamonds.png'),
-	  (10,'images/10_of_clubs.png'), (10,'images/10_of_hearts.png'), (10,'images/10_of_spades.png'), (10,'images/10_of_diamonds.png'),
-      (10,'images/jack_of_clubs.png'), (10,'images/jack_of_hearts.png'), (10,'images/jack_of_spades.png'), (10,'images/jack_of_diamonds.png'),
-      (10,'images/queen_of_clubs.png'), (10,'images/queen_of_hearts.png'), (10,'images/queen_of_spades.png'), (10,'images/queen_of_diamonds.png'),
-      (10,'images/king_of_clubs.png'), (10,'images/king_of_hearts.png'), (10,'images/king_of_spades.png'), (10,'images/king_of_diamonds.png')]
+def create_shoe():
+    global current_deck
+    deck=[(11,'images/ace_of_clubs.png'), (11,'images/ace_of_hearts.png'), (11,'images/ace_of_spades.png'), (11,'images/ace_of_diamonds.png'),
+          (2,'images/2_of_clubs.png'), (2,'images/2_of_hearts.png'), (2,'images/2_of_spades.png'), (2,'images/2_of_diamonds.png'),
+          (3,'images/3_of_clubs.png'), (3,'images/3_of_hearts.png'), (3,'images/3_of_spades.png'), (3,'images/3_of_diamonds.png'),
+	      (4,'images/4_of_clubs.png'), (4,'images/4_of_hearts.png'), (4,'images/4_of_spades.png'), (4,'images/4_of_diamonds.png'),
+	      (5,'images/5_of_clubs.png'), (5,'images/5_of_hearts.png'), (5,'images/5_of_spades.png'), (5,'images/5_of_diamonds.png'),
+	      (6,'images/6_of_clubs.png'), (6,'images/6_of_hearts.png'), (6,'images/6_of_spades.png'), (6,'images/6_of_diamonds.png'),
+	      (7,'images/7_of_clubs.png'), (7,'images/7_of_hearts.png'), (7,'images/7_of_spades.png'), (7,'images/7_of_diamonds.png'),
+	      (8,'images/8_of_clubs.png'), (8,'images/8_of_hearts.png'), (8,'images/8_of_spades.png'), (8,'images/8_of_diamonds.png'),
+	      (9,'images/9_of_clubs.png'), (9,'images/9_of_hearts.png'), (9,'images/9_of_spades.png'), (9,'images/9_of_diamonds.png'),
+	      (10,'images/10_of_clubs.png'), (10,'images/10_of_hearts.png'), (10,'images/10_of_spades.png'), (10,'images/10_of_diamonds.png'),
+          (10,'images/jack_of_clubs.png'), (10,'images/jack_of_hearts.png'), (10,'images/jack_of_spades.png'), (10,'images/jack_of_diamonds.png'),
+          (10,'images/queen_of_clubs.png'), (10,'images/queen_of_hearts.png'), (10,'images/queen_of_spades.png'), (10,'images/queen_of_diamonds.png'),
+          (10,'images/king_of_clubs.png'), (10,'images/king_of_hearts.png'), (10,'images/king_of_spades.png'), (10,'images/king_of_diamonds.png')]
 
-#create a shoe with 4 decks
-current_deck = deck.copy()
-current_deck.extend(current_deck)
-current_deck.extend(current_deck)
+    #create a shoe with 4 decks
+    current_deck = deck.copy()
+    current_deck.extend(current_deck)
+    current_deck.extend(current_deck)
 
 def reset_game():
     global hand
@@ -75,6 +80,8 @@ def reset_game():
     global draw
     global hand_over
     global first_hand
+    global player_ace_count
+    global dealer_ace_count
 
     hand = [None, None, None, None, None]
     dealer = [None, None, None, None, None]
@@ -93,6 +100,8 @@ def reset_game():
     draw = False
     hand_over = False
     first_hand = True
+    player_ace_count = 0
+    dealer_ace_count = 0
 
 def deal():
 
@@ -102,6 +111,9 @@ def deal():
     global player_score
     global dealer_score
     global first_hand
+    global player_won
+    global player_ace_count
+    global dealer_ace_count
 
     if not in_game and not player_stuck:
         reset_game()	
@@ -114,13 +126,18 @@ def deal():
         hand[0] = image
         current_deck.pop(next_card)
         player_score += curr_card[0]
+        if curr_card[0] == 11:
+            player_ace_count += 1
         #first card for dealer
+        next_card = random.randrange(0, len(current_deck))
         curr_card = current_deck[next_card]
         image = pygame.image.load(curr_card[1])
         image = pygame.transform.scale(image, (100, 150))
         dealer[0] = image
         current_deck.pop(next_card)
         dealer_score += curr_card[0]
+        if curr_card[0] == 11:
+            dealer_ace_count += 1
         #second card for player
         next_card = random.randrange(0, len(current_deck))
         curr_card = current_deck[next_card]
@@ -129,7 +146,13 @@ def deal():
         hand[1] = image
         current_deck.pop(next_card)
         player_score += curr_card[0]
-        in_game = True
+        if curr_card[0] == 11:
+            player_ace_count += 1
+
+        if player_score == 21:
+            player_won = True
+        else:
+            in_game = True
 
 def twist():
 
@@ -140,6 +163,7 @@ def twist():
     global player_bust
     global player_funds
     global hand_over
+    global player_ace_count
 
     if in_game and not player_stuck:
         i=0
@@ -152,15 +176,21 @@ def twist():
                 hand[i] = image
                 current_deck.pop(next_card)
                 player_score += curr_card[0]
+                if curr_card[0] == 11:
+                    player_ace_count += 1
                 break
             i+=1	
 
         if player_score > 21:
-            player_bust = True
-            player_funds -= player_stake
-            hand_over = True
-            in_game = False
-            player_stuck = False
+            if player_ace_count > 0:
+                player_ace_count -=1
+                player_score -= 10
+            else:
+                player_bust = True
+                player_funds -= player_stake
+                hand_over = True
+                in_game = False
+                player_stuck = False
 
 def stick():
 
@@ -174,6 +204,7 @@ def stick():
     global player_won
     global dealer_won
     global hand_over
+    global dealer_ace_count
 
     if in_game and not hand_over:
         player_stuck = True
@@ -187,12 +218,23 @@ def stick():
                 dealer[i] = image
                 current_deck.pop(next_card)
                 dealer_score += curr_card[0]
+                if curr_card[0] == 11:
+                    dealer_ace_count += 1
+
+                if dealer_score > 21:
+                    if dealer_ace_count > 0:
+                        dealer_ace_count -= 1
+                        dealer_score -= 10
                 if dealer_score >= 17:
                     break
             i+=1
         if dealer_score > 21:
-            dealer_bust = True
-            player_funds += player_stake
+            if dealer_ace_count > 0:
+                dealer_ace_count -= 1
+                dealer_score -= 10
+            else:
+                dealer_bust = True
+                player_funds += player_stake
         elif dealer_score == player_score:
             draw = True
         elif dealer_score > player_score:
@@ -274,12 +316,16 @@ def game_loop():
     global player_won
     global dealer_won
     global draw
+    global current_deck
  
     while not gameExit:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+
+        if len(current_deck) < 10:
+            create_shoe()
 
         gameDisplay.fill(bg_colour)
 
@@ -288,6 +334,7 @@ def game_loop():
         message_display(220,600,"You",70)
         message_display(1000,700,"Stake: £" + str(player_stake),20)
         message_display(1010,730,"Funds: £" + str(player_funds),20)
+        message_display(1010,760,"Cards: " + str(len(current_deck)),20)
 
         button("Deal",430,750,50,25,green,bright_green,deal)
         button("Twist",530,750,50,25,green,bright_green,twist) 
